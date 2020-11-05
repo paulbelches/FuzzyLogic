d3c9524ab6263720a982ae24b7d4b2ddbef443e6
tefabarrio <bar17927@uvg.edu.gt>
2020-11-04 20:33:52 -0600

    fuzzy logic done

--- gui.py
+++ gui.py
@@ -86,10 +86,9 @@ moving = True
 movingBall = False
 
 def valoresjug(ppx,ppy,pbx,pby,anglep):
-    x=abs(ppx-pbx)
-    y=abs(ppy-pby)
+    x=float(abs(ppx-pbx))
+    y=float(abs(ppy-pby))
     distance=math.sqrt((x*x+y*y))
-    anglep=math.degrees(anglep)
     if(ppx>pbx):
         angle1=math.degrees(math.atan(x/y))
         if(ppy>pby):
@@ -159,17 +158,114 @@ def anglebelongsto(angle):
         mucho=1
     return poco,maso,mucho
 
-# 1. si estoy muy volteado con respecto a la pelota, giro mucho
-# 2. si estoy medio volteado y lejos, giro un poco y avanzo muhco
-# 3. si estoy poco volteado y lejos, avanzo mucho
-# 4. si estoy medio lejos y medio volteado, giro un poco y avanzo un poco
-# 5. si estoy cerca y medio volteado, me volteo un poco
+
+def giromucho(x):
+    l1=360/4
+    l2=3*l1
+    valor=0
+    if(x<l1):
+        valor=0
+    elif(l1<=x<l2):
+        valor=((x-l1)/(l2-l1))
+    elif(l2<=1):
+        valor=1
+    return valor
+
+def giropoco(x):
+    l1=360/4
+    l2=3*l1
+    valor=0
+    if(x<l1):
+        valor=1
+    elif(l1<=x<l2):
+        valor=((x-l2)/(l1-l2))
+    elif(l2<=1):
+        valor=0
+    return valor
+
+def memuevomucho(x,Dmax):
+    l1=Dmax/4
+    l2=3*l1
+    valor=0
+    if(x<l1):
+        valor=0
+    elif(l1<=x<l2):
+        valor=((x-l1)/(l2-l1))
+    elif(l2<=1):
+        valor=1
+    return valor
+
+def memuevopoco(x,Dmax):
+    l1=Dmax/4
+    l2=3*l1
+    valor=0
+    if(x<l1):
+        valor=1
+    elif(l1<=x<l2):
+        valor=((x-l2)/(l1-l2))
+    elif(l2<=1):
+        valor=0
+    return valor
 
 
 def fuzzylogic(distance,angle,Dmax):
     cerca,medio,lejos=distancebelongsto(distance,Dmax)
     poco,maso,mucho=anglebelongsto(angle)
-    
+    list1=[]
+    list2=[]
+    list3=[]
+    list4=[]
+    # 1. si estoy muy volteado con respecto a la pelota, giro mucho
+    # 2. si estoy medio volteado y lejos, giro un poco y avanzo mucho
+    min2=min(maso,lejos)
+    # 3. si estoy poco volteado y lejos, avanzo mucho
+    # 4. si estoy medio volteado y medio lejos, giro un poco y avanzo un poco
+    min4=min(maso,medio)
+    # 5. si estoy medio volteado y cerca, me volteo un poco
+    min5=min(maso,cerca)
+    #PARA GIRAR
+    for i in range(0,360):
+        if(-0.01<giromucho(i)-mucho<0.01):
+            list1.append(i)
+            list2.append(giromucho(i))
+        if(-0.01<giropoco(i)-min2<0.01):
+            list1.append(i)
+            list2.append(giropoco(i))
+        if(-0.01<giropoco(i)-min4<0.01):
+            list1.append(i)
+            list2.append(giropoco(i))
+        if(-0.01<giropoco(i)-min5<0.01):
+            list1.append(i)
+            list2.append(giropoco(i))
+    suma=0
+    suma2=0
+    for k in range(len(list1)):
+        suma=suma+list1[k]*list2[k]
+        suma2=suma2+list2[k]
+    angulonuevo=suma/suma2
+    #PARA MOVERME     
+    for j in range(778):
+        if(-0.01<memuevomucho(j,Dmax)-min2<0.01):
+            list3.append(j)
+            list4.append(memuevomucho(j,Dmax))
+        min3=min(poco,lejos)
+        if(-0.01<memuevomucho(j,Dmax)-min3<0.01):
+            list3.append(j)
+            list4.append(memuevomucho(j,Dmax))
+        if(-0.01<memuevopoco(j,Dmax)-min4<0.01):
+            list3.append(j)
+            list4.append(memuevopoco(j,Dmax))
+    suma3=0
+    suma4=0
+    for l in range(len(list3)):
+        suma3=suma3+list3[l]*list4[l]
+        suma4=suma4+list4[l]
+    distancianueva=suma3/suma4
+    return angulonuevo,distancianueva
+
+#distance,angle=valoresjug(5,487,723,234,30)
+Dmax=math.sqrt(550*550*2)
+#angulonuevo,distancianueva=fuzzylogic(distance,angle,Dmax)
 
 
         
@@ -185,7 +281,10 @@ while True:
     screen.blit(ball.surf, ball.rect)
 
     if (moving):
-        moving = player.move(400, 1 * math.pi / 2, 5, 20) #poner los parametros
+
+        distance,angle=valoresjug(player.posx,player.posy,ball.posx,ball.posy,0)
+        angulonuevo,distancianueva=fuzzylogic(distance,angle,Dmax)
+        moving = player.move(distancianueva, math.radians(angle), 5, 20) #poner los parametros
         if(collition(player.posx, player.posy, ball.posx, ball.posy)):
             ball.setRect(player.posx + 50, player.posy + 25)
             moving = False
